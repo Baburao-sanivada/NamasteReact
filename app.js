b444dbdb65e42684e757ed17a42464258f3b9a92
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-09-11 08:51:14 +0530

    updated app.js

--- app.js
+++ app.js
@@ -33,33 +33,42 @@ const Header=()=>{
         </div>
     )
 }
-const Card=()=>{
+const Card=(props)=>{
+    const {data}=props;
     return (
         <div class="res-card">
             <img class="card-logo" alt="card-logo" src="https://b.zmtcdn.com/data/pictures/chains/4/94244/77aa082158f91099bc3738bd3623f77e.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"></img>
-            <h3>Chikoos Restaurant</h3>
-            <h4>Biriyani,Chinese</h4>
-            <h4>4.4 stars</h4>
+            <h3>{data.name}</h3>
+            <h4>{data.location}</h4>
+            <h4>{data.rating} stars</h4>
+            <h4>{data.time} min</h4>
         </div>
     )
 }
+const obj=[{
+    name:"chikoos Restaurant",
+    location:"Vizag",
+    rating:3.8,
+    time:30
+},{
+    name:"Nawabs",
+    location:"Vizag",
+    rating:3.8,
+    time:30
+},{
+    name:"Zeeshan",
+    location:"Vizag",
+    rating:3.8,
+    time:30
+}]
 const Body=()=>{
     return (
         <div className="res-container">
             <div className="search"><input type="search" placeholder="Biriyani on your Mind"></input>Search</div>
             <div className="card-container">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            {
+                obj.map((item)=><Card data={item}></Card>)
+            }
             </div>
         </div>
     )
