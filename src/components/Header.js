2c464e321286a907b10deab87efe87bb8e01b44c
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-09-12 10:08:26 +0530

    Cleaned The code and used Hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+import { LOGO_URL } from "../utils/constants";
 const Header=()=>{
     return (
         <div className="header">
-            <div className="logo"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThk6B4F2Q-aQEosit-VHmYfxhiDDAlBNo-JuA8nQN&s"/></div>
+            <div className="logo"><img src={LOGO_URL}/></div>
             <div className="nav-items">
                 <ul className="nav-list">
                     <li>Home</li>
--- src/utils/data.js
+++ src/utils/data.js
@@ -2,17 +2,21 @@ const obj=[{
     name:"chikoos Restaurant",
     location:"Vizag",
     rating:3.8,
-    time:30
+    time:30,
+    id:1
 },{
     name:"Nawabs",
     location:"Vizag",
-    rating:3.8,
-    time:30
+    rating:4.1,
+    time:30,
+    id:2
+
 },{
     name:"Zeeshan",
     location:"Vizag",
-    rating:3.8,
-    time:30
+    rating:4.5,
+    time:30,
+    id:3
 }]
 
 export default obj;
