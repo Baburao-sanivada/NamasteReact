615f58bb864c3e9a06d305fd89ad2bd4b6eaad0c
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-09-13 08:15:25 +0530

    Added Search Filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,11 +5,13 @@ export const Body=()=>{
     const [resList,SetresList]=useState(obj);
     return (
         <div className="res-container">
-            <div className="Filter">
+            <div className="Search">
+                <input type="text" id="inputtext" placeholder="Select Restaurant"></input>
                 <button className="filterButton" onClick={()=>{
-                    const updatedList=obj.filter((item)=> item.rating>4);
+                    console.log(document.getElementById("inputtext").value);
+                    const updatedList=obj.filter((item)=> item.name==document.getElementById("inputtext").value);
                     SetresList(updatedList);
-                }}>Top Rated Restaurants</button>
+                }}>Search</button>
             </div>
             <div className="card-container">
             {
