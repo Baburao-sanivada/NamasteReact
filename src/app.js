fcf295a164bc739bf20dd08563f5d09b88c4fe7a
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-09-19 15:27:39 +0530

    Used OutLet and Link

--- src/app.js
+++ src/app.js
@@ -2,15 +2,43 @@ import React  from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
+import Aboutus from "./components/Aboutus";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+
+
 
 const AppLayout=()=>{
     return (
         <div className="AppLayout">
             <Header></Header>
-            <Body></Body>
+            <Outlet/>
         </div>
     )
 }
 
+const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout></AppLayout>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>
+            },
+            {
+                path:"/aboutus",
+                element:<Aboutus/>
+            },
+            {
+                path:"/Contact",
+                element:<Contact/>
+            }
+        ],
+        errorElement:<Error></Error>
+    },
+    
+]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout></AppLayout>)
+root.render(<RouterProvider router={appRouter}/>)
--- src/components/Body.js
+++ src/components/Body.js
@@ -5,28 +5,32 @@ import { Shimmer } from "./Shimmer";
 
 
 export const Body=()=>{
+    const [ResList,SetResList]=useState([]);
     const [resList,SetresList]=useState([]);
 
+    const [InputVal,setInputValue]=useState("");
     useEffect(()=>{
         fetchData();
-        console.log("UseEffect Rendered");
     },[]);
-
     const fetchData=async () => {
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5071128&lng=78.35791119999999&collection=80441&tags=layout_CCS_Idli&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
         const json=await data.json();
-        console.log(json.data.cards.slice(3,));
         SetresList(json.data.cards.slice(3,));
+        SetResList(json.data.cards.slice(3,));
     }
     
     //Conditional Rendering
     return resList.length===0?<Shimmer></Shimmer>: (
         <div className="res-container">
             <div className="Search">
-                <input type="text" id="inputtext" placeholder="Select Restaurant"></input>
+                <input type="text" id="inputtext" placeholder="Select Restaurant" value={InputVal} onChange={(e)=>{
+                    setInputValue(e.target.value);
+                }}></input>
                 <button className="filterButton" onClick={()=>{
-                    console.log(document.getElementById("inputtext").value);
-                    const updatedList=obj.filter((item)=> item.name==document.getElementById("inputtext").value);
+                    console.log(ResList);
+                    const updatedList=ResList.filter((item)=>{
+                        return item.card.card.info.name.toLowerCase().includes(InputVal.toLowerCase());
+                    })
                     SetresList(updatedList);
                 }}>Search</button>
             </div>
